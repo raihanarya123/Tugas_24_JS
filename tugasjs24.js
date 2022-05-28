2ce2ba96041683b8b91035ed154a0f0b898fff75
Raihan Arya <raihanarya456@gmail.com>
2022-05-28 19:18:10 +0700

    Tugas  JS
    Tugas  JS

---
+++ tugasjs24.js
@@ -0,0 +1,7 @@
+function tugas24() {
+    var data = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+    data = data.sort((a, b) => a - b);
+    console.log(data);
+    console.log(data.reverse());
+}
+tugas24();
